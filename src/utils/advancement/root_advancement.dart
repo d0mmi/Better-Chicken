01ab4752e0f60c948c24c95a6997fed105af0ffc
Dominik Klysch <d.klysch@seeburger.de>
2020-01-22 15:56:47 +0100

    fixed root contructor

--- src/utils/advancement/root_advancement.dart
+++ src/utils/advancement/root_advancement.dart
@@ -7,7 +7,7 @@ import 'custom_advancement.dart';
 class RootAdvancement extends CustomAdvancement{
   String background;
 
-  RootAdvancement(String desc, ItemType icon, Map criteria,{this.background = "minecraft:textures/gui/advancements/backgrounds/stone.png"}) : super("root", desc, icon, criteria);
+  RootAdvancement(String desc, ItemType icon, Map criteria,{Map<String,dynamic> icon_nbt = null, AdvancementFrame frame = AdvancementFrame.task, bool show_toast = true, bool announce_to_chat = true, bool hidden = false,this.background = "minecraft:textures/gui/advancements/backgrounds/stone.png"}) : super("root", desc, icon, criteria, icon_nbt: icon_nbt, frame: frame, show_toast: show_toast, announce_to_chat: announce_to_chat, hidden: hidden);
 
 @override
   Map<String, dynamic> toMap() {
